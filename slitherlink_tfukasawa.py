0c5c28d0f65725b80adf33432d05b8194ec36c47
lcumaryo <Ellcy@Ellcy.local>
2016-02-12 16:43:50 +0900

    add dictionary

--- slitherlink_tfukasawa.py
+++ slitherlink_tfukasawa.py
@@ -6,6 +6,8 @@ print('Welcome to the SlitherLink ',M,'*',N,'!')
 
 #DEFINITION BEGIN
 
+numbers = {}    #DICTIONARY
+
 while True:     #DEF3
     print('Please enter the x-coordinate of a figure "3". Or enter "q" to go next step.')
     
@@ -26,12 +28,14 @@ while True:     #DEF3
             y = int(i)
             def number(x,y):
                 return 3
+            numbers[x,y] = 3
             print('(',x,',',y,') = ',number(x,y))
+
         else:
             print('You entered a wrong number.It must be 0 to ',N,'.')
 
-else:
-    print('You entered a wrong number.It must be 0 to ',M,'.')
+    else:
+        print('You entered a wrong number.It must be 0 to ',M,'.')
 
 
 while True:     #DEF2
@@ -54,12 +58,14 @@ while True:     #DEF2
             y = int(i)
             def number(x,y):
                 return 2
+            numbers[x,y] = 2
             print('(',x,',',y,') = ',number(x,y))
+
         else:
             print('You entered a wrong number.It must be 0 to ',N,'.')
 
-else:
-    print('You entered a wrong number.It must be 0 to ',M,'.')
+    else:
+        print('You entered a wrong number.It must be 0 to ',M,'.')
 
 while True:     #DEF1
     print('Please enter the x-coordinate of a figure "1". Or enter "q" to go next step.')
@@ -81,15 +87,17 @@ while True:     #DEF1
             y = int(i)
             def number(x,y):
                 return 1
+            numbers[x,y] = 1
             print('(',x,',',y,') = ',number(x,y))
+
         else:
             print('You entered a wrong number.It must be 0 to ',N,'.')
 
-else:
-    print('You entered a wrong number.It must be 0 to ',M,'.')
+    else:
+        print('You entered a wrong number.It must be 0 to ',M,'.')
 
 while True:     #DEF0
-    print('Please enter the x-coordinate of a figure "3". Or enter "q" to go next step.')
+    print('Please enter the x-coordinate of a figure "0". Or enter "q" to go next step.')
     
     i = input("")
     
@@ -108,18 +116,24 @@ while True:     #DEF0
             y = int(i)
             def number(x,y):
                 return 0
+            numbers[x,y] = 0
             print('(',x,',',y,') = ',number(x,y))
+
         else:
             print('You entered a wrong number.It must be 0 to ',N,'.')
 
-else:
-    print('You entered a wrong number.It must be 0 to ',M,'.')
+    else:
+        print('You entered a wrong number.It must be 0 to ',M,'.')
 
 col=[[1 for i in range(N)] for j in range(M+1)]
 
 row=[[1 for i in range(N+1)] for j in range(M)]
 
 #DEFINITION END
+
+#SOLVE BEGIN
+
+
 
 def link(s,t):
     if s<N and t<M:
@@ -150,6 +164,3 @@ def solved(a):
         print("unsolved")
 
 solved(allink(N+1,M+1))
-
-
-#JUDGEMENT
